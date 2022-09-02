ed6544ca11f65baf1016cf1b374a92232361c20e
Felix Buchholz <32803817+felixbuchholz@users.noreply.github.com>
2022-09-02 18:14:53 +0200

    feat(deploy): add deploy config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProduction ? '/next-react-d3-starter/' : '',
 }
 
 module.exports = nextConfig
