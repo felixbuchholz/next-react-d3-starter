79a375674b7e75c83034d42fb2b816fc38ec86cc
Felix Buchholz <32803817+felixbuchholz@users.noreply.github.com>
2022-09-02 17:38:13 +0200

    feat(readme): update readme and TestChart

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -7,7 +7,13 @@ export const Welcome: FC = () => {
   return (
     <Layout>
       <h1>A Next.js + D3 Starter!</h1>
-      <p>Ready for all your visualization needs.</p>
+      <em>Integrates D3 and React following a React first approach:</em>
+      <ul>
+        <li>DOM is entirely handled by React.</li>
+        <li>SVG elements and transitions are styled with CSS (modules).</li>
+        <li>Resizes Charts based on the D3 margins convention.</li>
+        <li>Tests with Jest and testing-library.</li>
+      </ul>
       <div>
         <TestChart />
       </div>
